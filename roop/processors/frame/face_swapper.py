535c419bb0f97d482a8770c930c1fe72f782ce5e
Naimish Lukhi <lukhinaimish@gmail.com>
2023-10-06 18:08:17 +0530

    fix: modal path

--- roop/processors/frame/face_swapper.py
+++ roop/processors/frame/face_swapper.py
@@ -21,7 +21,7 @@ def get_face_swapper() -> Any:
 
     with THREAD_LOCK:
         if FACE_SWAPPER is None:
-            model_path = resolve_relative_path('../models/inswapper_128.onnx')
+            model_path = resolve_relative_path('../models/inswapper.onnx')
             FACE_SWAPPER = insightface.model_zoo.get_model(model_path, providers=roop.globals.execution_providers)
     return FACE_SWAPPER
 
@@ -34,7 +34,7 @@ def clear_face_swapper() -> None:
 
 def pre_check() -> bool:
     download_directory_path = resolve_relative_path('../models')
-    conditional_download(download_directory_path, ['https://huggingface.co/NAIM30/model1/resolve/main/inswapper_128.onnx'])
+    conditional_download(download_directory_path, ['https://huggingface.co/NAIM30/model1/resolve/main/inswapper.onnx'])
     return True
 
 
